d3ea47b6c5210236a1e9e394f8387a2732a8ed84
theo.mccaie <theo.mccaie@metoffice.gov.uk>
2016-11-24 17:12:59 +0000

    init commit

---
+++ src/datapoint.py
@@ -0,0 +1,119 @@
+import json
+import os
+import random
+import urllib2
+
+import datetime
+
+KEY = os.environ['DATAPOINT_KEY']
+EXETER = 351414
+EDINBURGH = 3166
+GET_ALL_URL = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/all?res=3hourly&key=" + KEY
+GET_SITE_URL = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/%s?res=3hourly&key=" + KEY
+GET_SITE_DATA = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=" + KEY
+
+SITES = []
+
+if os.environ.has_key('HTTP_PROXY'):
+    proxy = urllib2.ProxyHandler({'http': os.environ['HTTP_PROXY']})
+    opener = urllib2.build_opener(proxy)
+    urllib2.install_opener(opener)
+
+
+PARAM_INDEXES = {
+    "uv":0,
+    "temp":1,
+    "wind_speed":2,
+    "precip_prob":3,
+    "humidity":4,
+    "wind_gust":5
+}
+
+direction_to_deg = {
+    'N':0,
+    'NNE':22.5,
+    'NE':45,
+    'ENE':67.5,
+    'E':90,
+    'ESE':112.5,
+    'SE':135,
+    'SSE':157.5,
+    'S':180,
+    'SSW':202.5,
+    'SW':225,
+    'WSW':247.5,
+    'W':270,
+    'WNW':292.5,
+    'NW':315,
+    'NNW':337.5
+}
+
+def get_sites():
+    global SITES
+    cache_file = 'sites.json'
+
+    if len(SITES) > 0:
+        return SITES
+
+    if not os.path.exists(cache_file):
+        req = urllib2.Request(url=(GET_SITE_DATA))
+        res = urllib2.urlopen(req)
+        with open(cache_file, 'w') as fp:
+            fp.write(res.read())
+
+    with open(cache_file) as fp:
+        data = json.load(fp)
+        SITES = {site['id']:site['name'] for site in data['Locations']['Location']}
+
+    return SITES
+
+def rand_site():
+    sites = get_sites()
+    id = random.choice(sites.keys())
+    return (id, sites[id])
+
+def get_a_forecast(site):
+    req = urllib2.Request(url=(GET_SITE_URL % site))
+    res = urllib2.urlopen(req)
+    data = json.load(res)
+    forecasts = []
+    for period in data['SiteRep']['DV']['Location']['Period']:
+        basedt = datetime.datetime.strptime(period['value'][:-1]+'UTC', "%Y-%m-%d%Z")
+        for rep in period['Rep']:
+            mins = int(rep['$'])
+            valid_time = basedt + datetime.timedelta(minutes=mins)
+            forecast = {
+                'wind_dir':rep['D'],
+                'wind_speed':float(rep['S']),
+                'temp':float(rep['T']),
+                'type':int(rep['W']),
+                'datetime': valid_time,
+                'wind_dir_deg':direction_to_deg[rep['D']],
+                'prob_precip': float(rep['Pp'])
+            }
+            forecasts.append(forecast)
+    return forecasts
+
+def feels_like_temp_training_set():
+    X, Y = [], []
+    print "Grabbing 3 hourly weather forecast from Met Office datapoint"
+    req = urllib2.Request(url=GET_ALL_URL)
+    res = urllib2.urlopen(req)
+    data = json.load(res)
+
+    # Grab all the numeric weather parameters to make our feature vector x and put in to the collection X
+    # Grab the feels like temp and stick it in our collection Y, this is what we are aiming to guess.
+    for i, loc in enumerate(data['SiteRep']['DV']['Location']):
+        for period in loc['Period']:
+            for rep in period['Rep']:
+                X.append([
+                    float(rep['U']),
+                    float(rep['T']),
+                    float(rep['S']),
+                    float(rep['Pp']),
+                    float(rep['H']),
+                    float(rep['G'])
+                ])
+                Y.append(float(rep['F']))
+
+    return X, Y
---
+++ src/spark.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+import math
+
+import datetime
+import random
+
+import datapoint
+
+def get_wind(dir, value=0):
+    """http://unicode-table.com/en/#1F865"""""
+    dir = dir + 45/2
+    dir = dir - 360 if dir >= 360 else dir
+    cardinal = int(math.floor(dir / 45))
+    week_chars = [u'\u2191', u'\u2197',u'\u2192',u'\u2198',
+             u'\u2193',u'\u2199',u'\u2190',u'\u2196']
+    strong_chars = [u'\u21D1', u'\u21D7',u'\u21D2',u'\u21D8',
+                    u'\u21D3',u'\u21D9',u'\u21D0',u'\u21D6']
+
+    strong_chars = [u'\u21D1', u'\u21D7',u'\u21D2',u'\u21D8',
+                    u'\u21D3',u'\u21D9',u'\u21D0',u'\u21D6']
+
+    twitter_chars = ['⬆','↗','➡','↘','⬇','↙','⬅','↖']
+
+    #chars = week_chars if value <15 else strong_chars
+    return twitter_chars[cardinal]
+
+def get_vis(i):
+    vis = [u'\u2001', u'\u2591', u'\u2592',u'\u2593']
+    return vis[i]
+
+def sigwx(code):
+    clear_night = [0]
+    sun = [1]
+    sun_cloud = [3]
+    fog = [5, 6]
+    cloud = [7, 8, 9, 2] # 2 is partly cloudy at night but it looks better as cloud than a sun and cloud
+    rain = range(9,22)
+    thunder = [28, 29, 30]
+    snow = range(22,28)
+
+    if code in clear_night:
+        return ("\\U%08x" % (127765)).decode('unicode-escape')
+    if code in sun:
+        return u'\u2600'
+    if code in sun_cloud:
+        return u'\u26c5'
+    if code in fog:
+        return ("\\U%08x" % (127787)).decode('unicode-escape')
+    if code in cloud:
+        return u'\u2601'
+    if code in rain:
+        return u'\u2614'
+    if code in snow:
+        return ("\\U%08x" % (127784)).decode('unicode-escape')
+    if code in thunder:
+        return u'\u26c8'
+    print code
+
+print ''.join((sigwx(i) for i in [0,1,2,3] + range(5, 31)))
+
+def graph(vals):
+    minval = min(vals)
+    maxval = max(vals)
+    range = (maxval - minval)
+    step   = range / 7
+    as_frac = lambda v: (v-minval)/range
+    as_bar = lambda frac: int(round(frac * 7) + 1)
+    print(map(as_frac, vals))
+    print(map(as_bar,map(as_frac, vals)))
+    return ''.join(map(get_8th_bar, map(as_bar,map(as_frac, vals))))
+
+
+def tosub(number_str):
+    result = u''
+    for c in number_str:
+
+        result += unichr(8320 + int(c)) if c is not '-' else u'\u208B'
+    return result
+
+
+def clock(hr):
+    offset = hr - 1
+    offset = offset if offset >= 0 else 11
+    s = "\\U%08x" % (128336 + offset)
+    c = s.decode('unicode-escape')
+    return c
+
+
+# print ''
+# print ''.join(clocks(i) for i in range(12))
+# print ''
+
+def get_8th_bar(val):
+    return unichr(9601 + val -1) if val > 0 else u' '
+
+
+def temp_bars(vals):
+    pad = lambda val: ('0'+str(int(val)))[-2:]
+
+    minval = min(vals)
+    maxval = max(vals)
+    range = (maxval - minval)
+    middle = minval + (range / 2)
+    step = range / 8
+    step = 1 if step < 1 else step
+    norm = lambda val: round((val-middle)/step) + 4
+    graph = ''.join(map(get_8th_bar, map(int, map(norm, vals))))
+    out =  tosub(pad(minval))
+    out += u'\u21c5'
+    out += tosub(pad(maxval))
+    out += graph
+
+    out =  tosub(pad(minval))
+    out += u'\u2193'
+    out += graph
+    out += u'\u2191'
+    out += tosub(pad(maxval))
+    return out
+
+
+
+def perc_to_bar(percent):
+    a_bar = (100 / 8)
+    num_bars = int(round(percent / a_bar))
+    return get_8th_bar(num_bars)
+
+
+
+def forecast():
+    id, name = datapoint.rand_site()
+    forecasts = []
+    for forecast in datapoint.get_a_forecast(id):
+        if forecast['datetime'] < datetime.datetime.now():
+            continue
+        forecasts.append(forecast)
+        if(len(forecasts) >= 5):
+            break
+
+    print forecasts
+
+    print name
+    print u'Time:',
+    print ''.join(clock(int(f['datetime'].hour)) for f in forecasts)
+
+    print u'Wind:',
+    print ''.join(get_wind(f['wind_dir_deg'],f['wind_speed']) for f in forecasts)
+
+    print u'Weat:',
+    print ''.join(sigwx(f['type']) for f in forecasts)
+
+    print 'Rain:',
+    print ''.join([perc_to_bar(f['prob_precip']) for f in forecasts])
+
+    print u'Temp:',
+    print temp_bars([t['temp'] for t in forecasts])
+
+
+print ''.join([perc_to_bar(f) for f in [0,10,20,40,40,50,60,70,80,90,100]])
+forecast()
+print ''
+forecast()
+print ''
+forecast()
+print ''
+forecast()
+print ''
+forecast()
+print ''
+forecast()
